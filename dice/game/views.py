13a24228f87cf7d1159feeee0117f5ae89434b0d
viethb <brittavieth@googlemail.com>
2021-01-23 16:49:41 +0100

    only calculate points when necessary

--- dice/game/views.py
+++ dice/game/views.py
@@ -19,16 +19,11 @@ def new_game(request, user_id):
     game_id = game.id
     return HttpResponseRedirect(reverse_lazy('roll',
                                              kwargs={'game_id': game_id}))
-    # return render(request, 'game/dice.html', {'page_title': 'Würfeln',
-    #                                           'game': game_id,
-    #                                           'dice': game.dice,
-    #                                           'dice_to_keep': 12345,
-    #                                           'scores': game.score_sheet,
-    #                                           'round': game.round % 4})
 
 
 def roll_dice(request, game_id, dice_to_keep=''):
     that_number = 400
+    # Punkte im score_sheet speichern
     if request.method == 'POST' and request.POST.get('points'):
         points = int(request.POST.get('points'))
         print(f' POINTS = {points}')
@@ -38,10 +33,10 @@ def roll_dice(request, game_id, dice_to_keep=''):
         if game.round % 4 != 0:
             game.round = game.round + (4 - game.round % 4)
         game.save()
-        messages.success(request, f'Eingetragen = {points}')
         return HttpResponseRedirect(reverse_lazy('roll',
                                                  kwargs={'game_id': game_id}))
 
+    # je nach Punktzahl den User speichern oder löschen, anschließend das Spiel löschen
     if request.method == 'POST' and request.POST.get('finish'):
         game = Game.objects.get(id=game_id)
         score = game.score_sheet['final_sum'] + that_number
@@ -60,61 +55,68 @@ def roll_dice(request, game_id, dice_to_keep=''):
         game.delete()
         return HttpResponseRedirect(reverse_lazy('get_score'))
 
-    if request.method == 'GET' and request.GET.get('roll'):
-        #dice_to_keep = request.GET.get('dice_to_keep')
-        dice_to_keep = '0'
-        if request.GET.get('keep_1'):
-            dice_to_keep += '1'
-        if request.GET.get('keep_2'):
-            dice_to_keep += '2'
-        if request.GET.get('keep_3'):
-            dice_to_keep += '3'
-        if request.GET.get('keep_4'):
-            dice_to_keep += '4'
-        if request.GET.get('keep_5'):
-            dice_to_keep += '5'
-
-    game = Game.objects.get(id=game_id)
-    dice = game.dice
-    scores = dict(game.score_sheet)
-
-    print(scores)
-    print(dice)
-
-    dice_list = [int(x) for x in dice_to_keep if x.isdigit()]
-    print(dice_list)
-
-    for i in range(len(dice)):
-        if i + 1 not in dice_list:
-            dice[i] = random.randint(1, 6)
-
-    numbers = count_numbers(dice)
-
-    for key in scores:
-        if scores[key] == 1000:
-            scores[key] = check_possible_points(game, numbers, key, that_number)
-
-    if request.method == 'GET':
+    # nicht ausgewählte Würfel würfeln und mögliche Punkte berechnen
+    if request.method == 'GET' :
+        game = Game.objects.get(id=game_id)
+        dice = game.dice
+        scores = dict(game.score_sheet)
         game.round = game.round + 1
-    game.dice = dice
-    game.save()
-    print(f'Scores-Variable: {scores}\n score_sheet: {game.score_sheet}')
+        dice_to_keep = '0'
 
-    images = ['img/die_0.png', 'img/die_1.png', 'img/die_2.png', 'img/die_3.png', 'img/die_4.png', 'img/die_5.png', 'img/die_6.png']
-    return render(request, 'game/dice.html', {'page_title': 'Würfeln',
-                                      'game': game_id,
-                                      'user': game.user.name,
-                                      'dice': dice,
-                                      'dice_to_keep': dice_to_keep,
-                                      'scores': scores,
-                                      'round': game.round % 4,
-                                      'number': that_number,
-                                      'images': images,
-                                      'upper': [('Einsen', 'ones'), ('Zweien', 'twos'), ('Dreien', 'threes'),
-                                                ('Vieren', 'fours'), ('Fünfen', 'fives'), ('Sechsen', 'sixes')],
-                                      'lower': [('Drilling', 'triple'), ('Vierling', 'quadruple'),
-                                                ('Full House', 'fullhouse'), ('Kl. Straße', 'small'),
-                                                ('Gr. Straße', 'big'), ('Kniffel', 'kniffel'), ('Chance', 'chance')]})
+        if game.round % 4 != 1:
+            if request.GET.get('keep_1'):
+                dice_to_keep += '1'
+            if request.GET.get('keep_2'):
+                dice_to_keep += '2'
+            if request.GET.get('keep_3'):
+                dice_to_keep += '3'
+            if request.GET.get('keep_4'):
+                dice_to_keep += '4'
+            if request.GET.get('keep_5'):
+                dice_to_keep += '5'
+
+            print(scores)
+            print(dice)
+
+            dice_list = [int(x) for x in dice_to_keep if x.isdigit()]
+            print(dice_list)
+
+            for i in range(len(dice)):
+                if i + 1 not in dice_list:
+                    dice[i] = random.randint(1, 6)
+
+            numbers = count_numbers(dice)
+
+            for key in scores:
+                if scores[key] == 1000:
+                    scores[key] = check_possible_points(game, numbers, key, that_number)
+
+            game.dice = dice
+        else:
+            for key in ['bonus', 'upper_sum', 'lower_sum', 'final_sum']:
+                if scores[key] == 1000:
+                    scores[key] = check_possible_points(game, [0, 0, 0, 0, 0, 0], key, that_number)
+        game.save()
+        print(f'Scores-Variable: {scores}\n score_sheet: {game.score_sheet}')
+
+        images = ['img/die_0.png', 'img/die_1.png', 'img/die_2.png', 'img/die_3.png', 'img/die_4.png', 'img/die_5.png',
+                  'img/die_6.png']
+        return render(request, 'game/dice.html', {'page_title': 'Würfeln',
+                                                  'game': game_id,
+                                                  'user': game.user.name,
+                                                  'dice': dice,
+                                                  'dice_to_keep': dice_to_keep,
+                                                  'scores': scores,
+                                                  'round': game.round % 4,
+                                                  'number': that_number,
+                                                  'images': images,
+                                                  'upper': [('Einsen', 'ones'), ('Zweien', 'twos'), ('Dreien', 'threes'),
+                                                            ('Vieren', 'fours'), ('Fünfen', 'fives'), ('Sechsen', 'sixes')],
+                                                  'lower': [('Drilling', 'triple'), ('Vierling', 'quadruple'),
+                                                            ('Full House', 'fullhouse'), ('Kl. Straße', 'small'),
+                                                            ('Gr. Straße', 'big'), ('Kniffel', 'kniffel'),
+                                                            ('Chance', 'chance')]})
+
 
 def count_numbers(dice):
     numbers = [0, 0, 0, 0, 0, 0]
@@ -143,13 +145,14 @@ def check_possible_points(game, numbers, category, that_number):
         print('In bonus')
         scores = game.score_sheet
         print(f'Scores: {scores}')
-        print(f'1000 not in list: {1000 not in {scores["ones"], scores["twos"], scores["threes"], scores["fours"], scores["fives"],scores["sixes"]}}')
+        print(
+            f'1000 not in list: {1000 not in {scores["ones"], scores["twos"], scores["threes"], scores["fours"], scores["fives"], scores["sixes"]}}')
         # prüft, ob alle oberen Kategorien ausgefüllt sind;
         if 1000 not in {scores['ones'], scores['twos'], scores['threes'], scores['fours'], scores['fives'],
                         scores['sixes']}:
             # wenn die Summe der oberen Punkte >= 63, wird der Bonus von 35 Punkten eingetragen
             if sum((scores['ones'], scores['twos'], scores['threes'], scores['fours'], scores['fives'],
-                    scores['sixes'], 6*that_number)) >= 63:
+                    scores['sixes'], 6 * that_number)) >= 63:
                 game.score_sheet['bonus'] = 35 - that_number
                 # game.save()
                 return 35 - that_number
@@ -199,13 +202,13 @@ def check_possible_points(game, numbers, category, that_number):
     elif category == 'lower_sum':
         scores = game.score_sheet
         lower_sum = sum((scores['triple'], scores['quadruple'], scores['fullhouse'], scores['small'], scores['big'],
-                        scores['kniffel'], scores['chance'], 7*that_number))
+                         scores['kniffel'], scores['chance'], 7 * that_number))
         print(f'lower_sum: {lower_sum}')
         if lower_sum < 1000:
             game.score_sheet['lower_sum'] = lower_sum - that_number
             return lower_sum - that_number
     elif category == 'final_sum':
-        final_sum = game.score_sheet['upper_sum'] + game.score_sheet['lower_sum'] + 2*that_number
+        final_sum = game.score_sheet['upper_sum'] + game.score_sheet['lower_sum'] + 2 * that_number
         if final_sum < 1000:
             game.score_sheet['final_sum'] = final_sum - that_number
             return final_sum - that_number
